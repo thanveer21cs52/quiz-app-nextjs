20563b9bc8057d8d795a15af95e002550cd70d3e
thanveer <thanveer21cs52@gmail.com>
2025-08-13 15:05:10 +0530

    fouth commit corrected the option issue

--- src/app/components/Singlequiz.tsx
+++ src/app/components/Singlequiz.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Cookies from "js-cookie";
 import { decode } from "html-entities";
 import { FormEvent, useContext, useEffect, useState } from "react";
@@ -9,204 +9,203 @@ import { useRouter } from "next/navigation";
 import { Resultcontext } from "./contexts/Finalresultcontext";
 import { useCountdown } from "../utils/Timer";
 
-
-
-
-function Singlequiz({quizdata}:{quizdata:any}) {
-const {seconds, active, reset, stop} =useCountdown()
-const [totalseconds,settotalseconds]=useState(0)
- const [isend,setend]=useState(false)
-  const router=useRouter()
- const [resultdata,setresultdata]=useState<any[]>(()=>{
-  if(typeof window !== "undefined"){
-      if(localStorage.getItem("resultdata")){
-    const parse=JSON.parse(localStorage.getItem("resultdata")||"[]")
-    return !isNaN(parse)?parse:[]
-  }
-  }
-
-  return []})
-  const [issubmit,setsubmit]=useState<boolean>(false)
-
-  const [page,setpage]=useState<number>(()=>{
-  if(typeof window !== "undefined"){
-      if(localStorage.getItem("page")){
-    const parse=JSON.parse(localStorage.getItem("page")||"1")
-    return !isNaN(parse)?parseInt(parse):1
-  }
-  }
-
-  return 1})
-
-  const [youranswer,setyouranswer]=useState({
-    id:"none",
-    option:"none"
-  })
-  let correctanswer={
-    id:"empty",
-    option:"empty"
-  }
-  
-  
-  const {Result,setResult}=useContext(Resultcontext)
-  const {score,setScore}=useContext(Scorecontext)
-
-   function checkanswer(event:any){
-    event.preventDefault(); 
-     
-   
-
-    if(issubmit){
-      const formData = new FormData(event.target);
-  const answer = formData.get(`${page}`);
-  if(youranswer==correctanswer){
-    setScore(score+1)
-     
-  }
-  console.log(correctanswer,youranswer,"correct","your")
-  console.log(quizdata)
-  Cookies.set("score", `${score}`, { expires:1/144  })
-
+function Singlequiz({ quizdata }: { quizdata: any }) {
+  const { seconds, active, reset, stop } = useCountdown();
+  const [totalseconds, settotalseconds] = useState(0);
+  const [isend, setend] = useState(false);
+  const router = useRouter();
+  const [resultdata, setresultdata] = useState<any[]>(() => {
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("resultdata")) {
+        const parse = JSON.parse(localStorage.getItem("resultdata") || "[]");
+        return !isNaN(parse) ? parse : [];
+      }
     }
 
-  
- 
+    return [];
+  });
+  const [issubmit, setsubmit] = useState<boolean>(false);
 
-  
+  const [page, setpage] = useState<number>(() => {
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("page")) {
+        const parse = JSON.parse(localStorage.getItem("page") || "1");
+        return !isNaN(parse) ? parseInt(parse) : 1;
+      }
+    }
 
-
-  
-
-   }
-   function handlesubmit(){
-   
-  stop(); 
-
-  settotalseconds(prev => prev + (10 - seconds)); 
-
-  const updatedAnswer = { ...quizdata[page - 1], youranswer };
-
-
-  const isCorrect = correctanswer.id === youranswer.id;
-
-  if (isCorrect) {
-        localStorage.setItem("scoredata", JSON.stringify(score+1))
-    setScore(prev => prev + 1);
-  }
-
- 
-  setresultdata(prev => {
-    const newResult = [...prev, updatedAnswer];
-    localStorage.setItem("resultdata", JSON.stringify(newResult));
-    return newResult;
+    return 1;
   });
 
-  if (page === 10) {
-    setend(true); 
-    return;
-  }
+  const [youranswer, setyouranswer] = useState({
+    id: "none",
+    option: "none",
+  });
+  let correctanswer = {
+    id: "empty",
+    option: "empty",
+  };
 
-  setsubmit(true); 
-}
+  const { Result, setResult } = useContext(Resultcontext);
+  const { score, setScore } = useContext(Scorecontext);
 
-     function handleNext() {
-     
-      setpage(page+1)
-      setyouranswer({id:"none",option:"none"})
- setsubmit(false)
+  function checkanswer(event: any) {
+    event.preventDefault();
+
+    if (issubmit) {
+      const formData = new FormData(event.target);
+      const answer = formData.get(`${page}`);
+      if (youranswer == correctanswer) {
+        setScore(score + 1);
+      }
+      console.log(correctanswer, youranswer, "correct", "your");
+      console.log(quizdata);
+      Cookies.set("score", `${score}`, { expires: 1 / 144 });
+    }
   }
-  useEffect(()=>{
-    if(isend){
-      setResult(resultdata)
- 
-      router.push(`/quizes/results?totalseconds=${totalseconds}`)
-      
-       
-    
+  function handlesubmit() {
+    stop();
+
+    settotalseconds((prev) => prev + (10 - seconds));
+
+    const updatedAnswer = { ...quizdata[page - 1], youranswer };
+
+    const isCorrect = correctanswer.id === youranswer.id;
+
+    if (isCorrect) {
+      localStorage.setItem("scoredata", JSON.stringify(score + 1));
+      setScore((prev) => prev + 1);
     }
 
-  },[isend])
-  useEffect(()=>{
-    
-    reset()
-    localStorage.setItem("page",JSON.stringify(page))
-    console.log(totalseconds,"sdss")
-  },[page])
+    setresultdata((prev) => {
+      const newResult = [...prev, updatedAnswer];
+      localStorage.setItem("resultdata", JSON.stringify(newResult));
+      return newResult;
+    });
 
-useEffect(() => {
-  if (seconds === 0 && !issubmit) {
-    handlesubmit();
+    if (page === 10) {
+      setend(true);
+      return;
+    }
+
+    setsubmit(true);
   }
-}, [seconds]);
- 
-   
 
- 
-  
+  function handleNext() {
+    setpage(page + 1);
+    setyouranswer({ id: "none", option: "none" });
+    setsubmit(false);
+  }
+  useEffect(() => {
+    if (isend) {
+      setResult(resultdata);
 
-    return (
-   
-    <div className=" bg-quizbg text-quizbutton p-6 flex flex-col space-y-4 justify-center items-start h-fit w-2/4" >
-            <div className="flex w-full justify-between">
-             
-              <div className="text-quiztimer flex justify-start ">{seconds==0&&"Times Up Go Next"}</div>
-              <div className="text-quiztimer flex justify-end">Timer: 00:{seconds!=10&&0}{seconds}</div></div>
-            <div className="flex flex-col justify-center items-start space-y-3  ">
-               {page} {decode(quizdata[page-1].question)}
-             </div>
-       
-             <div className="flex  flex-col space-y-2 w-full ">
-             {quizdata[page-1].options.map((option:any,index:number)=>{
-              if(decode(option)==decode(quizdata[page-1].correct_answer
-)){
-                correctanswer={id:`${page-1}-${index}`,
-                  option:decode(option)
-                }
-              }
-              const optionid=`${page-1}-${index}`
-                 return <label htmlFor={optionid} key={optionid} className={`flex ${optionid==correctanswer.id&&issubmit&&youranswer.id!="none"&&"bg-green-400"} ${correctanswer.id==youranswer.id&&issubmit&&optionid==youranswer.id&&"bg-green-400"} ${optionid==youranswer.id&&issubmit&&optionid!=correctanswer.id&&"bg-red-400"} p-2 border-quizbutton border-2 space-x-2 shadow-black ${!issubmit&&"hover:bg-quizhover"} ${issubmit&&"text-black/30"} `}>
-                   <input type="radio" id={optionid} name={`${page-1}`} value={decode(option)} disabled={seconds==0||issubmit} onClick={(e:any)=>{
-                    setyouranswer({
-                      id:`${e.target.id}`,
-                      option:e.target.value
-                    })
-                   }}/>
-                   <div >
-                   {decode(option)}</div>
-                 </label>
-               
-             })}
-       
-             </div>
-           
-       
-             <div className="flex justify-center items-center w-full  mt-5">
-               <button className="text-sm bg-quizbutton w-3/4 text-gray-50  py-2 rounded-sm cursor-pointer hover:bg-amber-800" type="button"  onClick={()=>{
-               if(!issubmit&&seconds==0){
-                handlesubmit()
-               }
-               else if(issubmit){
-                handleNext()
-               }else if(!issubmit&&seconds!=0){
-                handlesubmit()
-               }else if(seconds==0){
-                handlesubmit()
-               }
+      router.push(`/quizes/results?totalseconds=${totalseconds}`);
+    }
+  }, [isend]);
+  useEffect(() => {
+    reset();
+    localStorage.setItem("page", JSON.stringify(page));
+    console.log(totalseconds, "sdss");
+  }, [page]);
 
-               
-               }}>
-                 {(issubmit)?"next":"submit"}
-               </button> 
-               
-             
-             </div>
-           </div>
-      
- 
-        
-     
-        
-    );
+  useEffect(() => {
+    if (seconds === 0 && !issubmit) {
+      setyouranswer({
+        id:"none",
+        option:"none"
+      })
+      handlesubmit();
+    }
+  }, [seconds]);
+
+  return (
+    <div className=" bg-quizbg text-quizbutton p-6 flex flex-col space-y-4 justify-center items-start h-fit w-2/4">
+      <div className="flex w-full justify-between">
+        <div className="text-quiztimer flex justify-start ">
+          {seconds == 0 && "Times Up Go Next"}
+        </div>
+        <div className="text-quiztimer flex justify-end">
+          Timer: 00:{seconds != 10 && 0}
+          {seconds}
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-start space-y-3  ">
+        {page} {decode(quizdata[page - 1].question)}
+      </div>
+
+      <div className="flex  flex-col space-y-2 w-full ">
+        {quizdata[page - 1].options.map((option: any, index: number) => {
+          if (decode(option) == decode(quizdata[page - 1].correct_answer)) {
+            correctanswer = {
+              id: `${page - 1}-${index}`,
+              option: decode(option),
+            };
+          }
+          const optionid = `${page - 1}-${index}`;
+          return (
+            <label
+              htmlFor={optionid}
+              key={optionid}
+              className={`flex ${
+                optionid == correctanswer.id &&
+                issubmit &&
+                youranswer.id != "none" &&
+                "bg-green-400"
+              } ${
+                correctanswer.id == youranswer.id &&
+                issubmit &&
+                optionid == youranswer.id &&
+                "bg-green-400"
+              } ${
+                optionid == youranswer.id &&
+                issubmit &&
+                optionid != correctanswer.id &&
+                "bg-red-400"
+              } p-2 border-quizbutton border-2 space-x-2 shadow-black ${
+                !issubmit && "hover:bg-quizhover"
+              } ${issubmit && "text-black/30"} `}
+            >
+              <input
+                type="radio"
+                id={optionid}
+                name={`${page - 1}`}
+                value={decode(option)}
+                disabled={seconds == 0 || issubmit}
+                onClick={(e: any) => {
+                  setyouranswer({
+                    id: `${e.target.id}`,
+                    option: e.target.value,
+                  });
+                }}
+              />
+              <div>{decode(option)}</div>
+            </label>
+          );
+        })}
+      </div>
+
+      <div className="flex justify-center items-center w-full  mt-5">
+        <button
+          className="text-sm bg-quizbutton w-3/4 text-gray-50  py-2 rounded-sm cursor-pointer hover:bg-amber-800"
+          type="button"
+          onClick={() => {
+            if (!issubmit && seconds == 0) {
+              handlesubmit();
+            } else if (issubmit) {
+              handleNext();
+            } else if (!issubmit && seconds != 0) {
+              handlesubmit();
+            } else if (seconds == 0) {
+              handlesubmit();
+            }
+          }}
+        >
+          {issubmit ? "next" : "submit"}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Singlequiz;
